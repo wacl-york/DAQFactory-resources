f394651efb1b68e663d692de73707d5ae8bcc4f2
willdrysdale <wsd500@york.ac.uk>
2024-03-28 13:48:08 +0000

    Create wacldaq
    R package for generating basic logging scripts for the instruments covered by the COZI Logger. Some assembly required

---
+++ wacldaq/R/aaa.R
@@ -0,0 +1,5 @@
+# Environment for storing the instrument table to prevent multiple google sheets api calls
+
+the = new.env(parent = emptyenv())
+
+the$instruments = NULL
---
+++ wacldaq/R/get_instrument_table.R
@@ -0,0 +1,38 @@
+#' Get Instrument Table
+#'
+#' Returns a tibble containing name, ip, port and serial for all the instruments
+#' support by the logger
+#' Right now this downloads the cozi_instruments spreadsheet from google drive.
+#' The user needs have "cozi_instruments_id" as the sheets_id in {{keyring}} such that
+#' \code{keyring::key_get("cozi_instruments_id")} returns the sheet id.
+#' This will certainly change when the instrument data finds a better home
+#'
+#' @param keyName string default cozi_instruments_id. lets the user override the name of their stored key
+#' @param force boolean default false. When true re-download the instrument table even if it is cached in the package environment
+#'
+#' @author W. S. Drysdale
+#'
+#' @export
+
+get_instrument_table = function(keyName = "cozi_instruments_id",
+                                force = F){
+
+  if(is.null(the$allInstruments) | force){
+    sheetID = keyring::key_get(keyName)
+
+    sheet = googlesheets4::read_sheet(sheetID, "Sheet1")
+
+    allInstruments = sheet |>
+      dplyr::select(
+        name = Katie_simple_name,
+        serial = Serial_number,
+        ip = IP_Address,
+        port = Port) |>
+      dplyr::filter(dplyr::if_all(dplyr::everything(), ~!is.na(.x))) # only support instruments that have all variables present
+
+    the$allInstruments = allInstruments
+  }
+
+  allInstruments
+
+}
